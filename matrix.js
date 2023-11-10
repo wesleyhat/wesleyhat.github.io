5f16e65f898b3195b7d20db04eb870ada4c0d212
wesleyhat <80120977+wesleyhat@users.noreply.github.com>
2023-11-10 10:21:06 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -46,3 +46,28 @@ document.body.onkeyup = function(e) {
         setInterval(draw, 30);
     }
   }
+
+canvas.addEventListener("touchstart", process_touchstart, false);
+
+function process_touchstart(ev){
+    const draw = () => {
+            context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+        
+            context.fillStyle = '#0F0';
+            context.font = fontSize + 'px monospace';
+        
+            for(let i = 0; i < rainDrops.length; i++)
+            {
+                const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+                context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+        
+                if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+                    rainDrops[i] = 0;
+                }
+                rainDrops[i]++;
+            }
+        };
+        
+        setInterval(draw, 30);
+}
