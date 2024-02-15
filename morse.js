3771cc4d99f52b2e3041c3c916d387e12209aec2
wesleyhat <80120977+wesleyhat@users.noreply.github.com>
2024-02-14 19:26:21 -0500

    Update morse.js

--- morse.js
+++ morse.js
@@ -1,104 +1,269 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no viewport-fit=cover"">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Victor+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+const dictionary = ["gips","phon","slit","they","rebs","abye","tret","cues","lips","jots","that","zing","ebbs","vain","ates","meld","jows","wick","rued","taxi","park","dows","rust","seta","falx","vavs","vina","fete","clam","caph","wyle","name","olla","oars","kame","open","kaas","hear","tear","list","cusk","jays","bias","crow","reap","bola","sing","coca","ewes","exec","mols","geta","purr","tuis","veer","slue","dice","snow","sulu","amir","huge","tort","jibs","imid","oust","taut","chaw","gout","lope","tags","shiv","hove","waws","zoos","core","twas","pyic","lead","helm","tote","drum","kink","yags","tine","poos","spue","vatu","flat","conk","haul","peke","unde","waps","kolo","king","scot","snug","feat","cake","said","bast","naos","tosh","flub","arco","sipe","lion","file","sabe","uric","drys","play","info","moxa","vive","hare","tola","clip","wage","vied","ride","such","cant","omer","soli","scry","cars","vans","bets","marl","smew","cobs","dots","ofay","jerk","dexy","lobo","fila","hemp","pirn","quid","ruly","loop","foam","lunk","come","raws","whoa","wich","luna","eger","doat","tots","fizz","axed","haar","sugh","mace","etui","zags","nome","osar","luxe","pula","jive","lice","horn","ergs","lord","germ","seek","airn","bang","ptui","yarn","vote","peal","perv","yaup","keef","dugs","oyes","ayes","gaps","jock","soma","fear","pale","jags","acre","ever","pfft","unai","iota","some","user","megs","gran","zees","enow","nits","wham","emus","kens","gage","dork","fall","feeb","cove","blob","sold","pass","deep","smit","haze","rose","lees","wart","yald","fish","fano","tolu","woes","lour","dive","jury","vacs","ding","ouch","grab","sage","eyne","head","loft","gain","rape","gent","caps","guns","tows","anga","thus","urps","gall","rhus","star","lych","lent","sora","whup","sris","vend","shim","rush","bode","skip","poms","nada","loam","paik","ouph","sons","auto","onyx","mixt","zine","pump","slaw","nurd","taps","sods","tres","oxes","redd","pubs","harm","tael","casa","wash","bags","syce","efts","apos","anew","apex","flap","dyad","jane","alme","dock","hies","span","veal","sack","hick","waft","hint","army","mags","watt","dunk","bort","rite","brrr","dure","ruth","whew","blot","gyre","limy","jole","coos","tegg","geez","soak","dirk","coni","echt","west","wets","dopy","rave","soba","glow","obes","sows","anal","vise","ogee","konk","roll","bach","fife","matt","impi","hath","ditz","onos","hake","anti","libs","slip","gape","lake","duit","keps","subs","walk","hyla","mien","spam","thru","yeps","fowl","iwis","suit","roam","robe","upby","spin","idyl","disk","barf","node","blow","ritz","ruse","maes","dits","ogam","lies","live","fled","stay","burp","ashy","sags","delt","baff","maya","nans","prog","veto","coly","koto","dado","clan","vees","yous","vets","narc","paws","dose","plat","zits","reps","riff","quag","nays","weal","hoya","chez","yegg","tass","laps","doss","fast","davy","lall","piny","sumo","jets","awee","kina","fine","tele","alef","sell","true","seel","vole","wily","fabs","fets","rani","ants","tori","mity","temp","kyar","yuan","nark","dude","plug","liny","fora","redo","keto","hows","crux","bolo","darb","mutt","dels","ache","owed","kits","trad","mora","pacy","nett","gaur","awny","ears","etna","prat","read","inby","deck","surf","swum","pith","ichs","undo","tony","liar","halo","died","eyed","eyer","tung","oldy","cere","hade","ally","sour","cote","sord","craw","wane","zein","duds","thro","fugs","viny","slim","call","goop","pelf","yule","boar","birr","staw","byrl","hems","lulu","orgy","wack","corm","fool","junk","auld","puny","tews","axal","fico","tyke","coil","didy","tome","jigs","ajee","baud","erst","oots","flam","merk","guts","melt","bone","kapa","beep","dint","goad","bean","firm","whim","khet","life","blur","idem","boys","mist","whap","defy","raft","took","dawn","dhow","ploy","comb","want","saki","noms","gobo","frug","muss","twee","arvo","alec","duck","tavs","keep","rocs","aria","newt","mors","mean","silt","drat","guls","bond","mums","lots","goof","hula","knar","chub","seif","jamb","cods","mead","ired","amah","rood","kohl","bear","bice","pact","oops","thin","obey","area","poke","diol","josh","wino","fane","lewd","woof","apps","lisp","sand","porn","awry","cats","neep","kafs","spik","dart","wigs","moor","yurt","stow","gins","pomo","pima","urus","scud","mach","giro","waxy","dhak","jabs","eaux","mice","phat","boll","duro","tory","yoke","mack","cols","dore","pend","clue","eath","obis","knob","tide","egad","unau","gaen","guan","mesa","talk","rube","turk","scup","gate","gyms","eddo","nisi","bumf","tapa","proa","sene","duma","wove","oaks","rain","leud","tors","hame","duns","fuse","bids","gong","dura","scad","bars","welt","clef","pack","baal","heft","bill","deke","souk","peon","vigs","taco","yutz","vair","deil","also","ghee","vagi","zonk","bilk","reed","brit","foxy","peas","wile","bait","hurl","pfui","mage","elhi","froe","dole","acme","doth","owls","rend","lati","acyl","joey","zori","tuft","folk","chow","palm","woos","food","mids","brat","lime","salt","oohs","ulva","deme","sops","char","pule","utes","miso","rake","yeld","trey","jeer","slap","weft","vier","uvea","gaed","cuss","taro","torc","tuns","lobe","nims","quip","ibex","sibb","tire","grub","hunt","nana","negs","sard","hern","arty","taws","meek","hoke","tzar","zeds","fist","yuck","lota","lady","buck","ares","sick","mule","neve","zigs","lams","kabs","brad","cony","fids","latu","opal","earl","fact","turn","pone","leke","tail","next","nows","fest","mews","heal","curf","garb","semi","corf","fays","luge","oral","drib","hate","hail","pyes","prim","cows","mole","yolk","axil","aura","fohn","egos","deed","maar","alae","pele","hags","keel","bosh","huts","ryke","rums","poor","tame","yups","whey","lied","blah","posh","lost","jeep","unco","crap","neuk","midi","bite","went","aver","luvs","tens","chug","bush","volt","alts","frae","enuf","oozy","agio","sari","holm","hire","slew","pans","trow","dell","draw","parr","pics","plow","blub","rich","rias","weep","cark","kiln","repp","buoy","hoer","nuke","dims","enol","meat","ocas","peag","girt","elms","sall","flan","gama","nona","labs","muds","ecus","flex","dahs","wilt","dons","goon","gaes","tabs","viva","weak","muse","jail","dent","holt","geld","amia","mons","leks","haet","tusk","yens","habu","haed","crew","verb","caul","plot","twae","ages","yins","toke","tale","auks","piki","rack","ahoy","gays","lens","eery","milt","kayo","mine","paly","yips","chia","nidi","bedu","dome","roux","cola","loom","gite","jeez","toff","kiss","bulb","pyro","fork","gyve","puja","yobs","tore","data","lust","frag","whid","tuff","ores","ayin","hoax","gasp","dolt","jins","offs","pigs","vims","logs","sexy","fisc","meou","oxen","pung","bork","unto","them","ceca","ajar","feds","tain","boon","unbe","leas","cory","vasa","hadj","sice","weer","tabu","hyps","text","more","cwms","pegs","ewer","pows","wave","chip","lint","gang","obit","away","brow","quiz","raja","bony","soph","doum","orcs","knit","good","anil","foss","tyin","sago","wady","weld","grad","than","lash","dhal","pins","jimp","lest","acid"]
+const morseCodeDictionary = {
+    'a': '.-', 'b': '-...',
+    'c': '-.-.', 'd': '-..', 'e': '.',
+    'f': '..-.', 'g': '--.', 'h': '....',
+    'i': '..', 'j': '.---', 'k': '-.-',
+    'l': '.-..', 'm': '--', 'n': '-.',
+    'o': '---', 'p': '.--.', 'q': '--.-',
+    'r': '.-.', 's': '...', 't': '-',
+    'u': '..-', 'v': '...-', 'w': '.--',
+    'x': '-..-', 'y': '-.--', 'z': '--..'
+};
+let wordDisplay = document.getElementById("wordDisplay");
+let txtBox = document.getElementById("txtBox");
+let btnSubmit = document.getElementById("btnSubmit");
+let btnReset = document.getElementById("btnReset");
+let result = document.getElementById("result");
+let tempGuess = "";
+let currentWord;
+let morseCode
 
-    <link rel="stylesheet" href="morsecodestyles.css">
-    <title>Learn Morse Code</title>
-</head>
-<body>
-    <div class="top">
-        <h1>Learn Morse Code</h1>
-    </div>
+function wordToMorseCode(word) {
+    return word.split('').map(char => {
+      if (morseCodeDictionary[char]) {
+        return morseCodeDictionary[char];
+      } else {
+        return char;
+      }
+    }).join(' ');
+} 
 
-<div class="keypad">
-  <div class="key" id="key">
-    <div class="grid">
-        <p class="ltr">A: .-</p>
-        <p class="ltr">B: -...</p>
-        <p class="ltr">C: -.-.</p>
-        <p class="ltr">D: -..</p>
-        <p class="ltr">E: .</p>
-        <p class="ltr">F: ..-.</p>
-        <p class="ltr">G: --.</p>
-        <p class="ltr">H: ....</p>
-        <p class="ltr">I: ..</p>
-        <p class="ltr">J: .---</p>
-        <p class="ltr">K: -.-</p>
-        <p class="ltr">L: .-..</p>
-        <p class="ltr">M: --</p>
-        <p class="ltr">N: -.</p>
-        <p class="ltr">O: ---</p>
-        <p class="ltr">P: .--.</p>
-        <p class="ltr">Q: --.-</p>
-        <p class="ltr">R: .-.</p>
-        <p class="ltr">S: ...</p>
-        <p class="ltr">T: -</p>
-        <p class="ltr">U: ..-</p>
-        <p class="ltr">V: ...-</p>
-        <p class="ltr">W: .--</p>
-        <p class="ltr">X: -..-</p>
-        <p class="ltr">Y: -.--</p>
-        <p class="ltr">Z: --..</p>
-    </div>
-</div>
-</div>
+function displayWord(){
+    let word = dictionary[Math.floor(Math.random() * dictionary.length)];
 
-    <div class="container">
-        <h2 class="word" id="wordDisplay">Test Word</h2>
-        <div class="user-input">
-            
-            <div id="txtBox" class="txtBox"></div>
-            <button class="btn" id="btnSubmit">Submit</button>
-        </div>
+    let newWord = word;
+    // let newWord = "hhhh";
 
-        <div class="keyboard-container">
-            <div class="row">
-              <div class="letter letter1" id="q">Q</div>
-              <div class="letter letter2" id="w">W</div>
-              <div class="letter letter3" id="e">E</div>
-              <div class="letter letter4" id="r">R</div>
-              <div class="letter letter5" id="t">T</div>
-              <div class="letter letter6" id="y">Y</div>
-              <div class="letter letter7" id="u">U</div>
-              <div class="letter letter8" id="i">I</div>
-              <div class="letter letter9" id="o">O</div>
-              <div class="letter letter10" id="p">P</div>
-            </div>
-            <div class="row">
-              <div class="letter letter11" id="a">A</div>
-              <div class="letter letter12" id="s">S</div>
-              <div class="letter letter13" id="d">D</div>
-              <div class="letter letter14" id="f">F</div>
-              <div class="letter letter15" id="g">G</div>
-              <div class="letter letter16" id="h">H</div>
-              <div class="letter letter17" id="j">J</div>
-              <div class="letter letter18" id="k">K</div>
-              <div class="letter letter19" id="l">L</div>
-            </div>
-            <div class="row">
-              <div class="letterButton letter20" id="enter"><p>ENTER</p></div>
-              <div class="letter letter21" id="z">Z</div>
-              <div class="letter letter22" id="x">X</div>
-              <div class="letter letter23" id="c">C</div>
-              <div class="letter letter24" id="v">V</div>
-              <div class="letter letter25" id="b">B</div>
-              <div class="letter letter26" id="n">N</div>
-              <div class="letter letter27" id="m">M</div>
-              <div class="letterButton letter28" id="back"><p>BACK</p></div>
-            </div>
-          </div>
-        </div>
-        
-    </div>
+    currentWord = newWord;
+
+    morseCode = wordToMorseCode(newWord);
+
+    wordDisplay.innerHTML = morseCode
+
+    console.log(currentWord)
+}
+
+btnSubmit.addEventListener('click', () => {
+
+    submitGuess();
+});
+
+function getKeys(){
+    let q = document.getElementById('q');
+    let w = document.getElementById('w');
+    let e = document.getElementById('e');
+    let r = document.getElementById('r');
+    let t = document.getElementById('t');
+    let y = document.getElementById('y');
+    let u = document.getElementById('u');
+    let i = document.getElementById('i');
+    let o = document.getElementById('o');
+    let p = document.getElementById('p');
+    let a = document.getElementById('a');
+    let s = document.getElementById('s');
+    let d = document.getElementById('d');
+    let f = document.getElementById('f');
+    let g = document.getElementById('g');
+    let h = document.getElementById('h');
+    let j = document.getElementById('j');
+    let k = document.getElementById('k');
+    let l = document.getElementById('l');
+    let enter = document.getElementById('enter');
+    let z = document.getElementById('z');
+    let x = document.getElementById('x');
+    let c = document.getElementById('c');
+    let v = document.getElementById('v');
+    let b = document.getElementById('b');
+    let n = document.getElementById('n');
+    let m = document.getElementById('m');
+    let back = document.getElementById('back');
+  
+    keyElement(q);
+    keyElement(w);
+    keyElement(e);
+    keyElement(r);
+    keyElement(t);
+    keyElement(y);
+    keyElement(u);
+    keyElement(i);
+    keyElement(o);
+    keyElement(p);
+    keyElement(a);
+    keyElement(s);
+    keyElement(d);
+    keyElement(f);
+    keyElement(g);
+    keyElement(h);
+    keyElement(j);
+    keyElement(k);
+    keyElement(l);
+    keyElement(z);
+    keyElement(x);
+    keyElement(c);
+    keyElement(v);
+    keyElement(b);
+    keyElement(n);
+    keyElement(m);
+  
+}
+
+function keyElement(element) {
+    element.addEventListener('click', () => {
+        let letter = element.innerText;
+        const key = letter.toLowerCase();
+
+        tempGuess += key
+
+        txtBox.innerHTML = tempGuess
+        return;
+    });
+}
+
+function registerKeyboardEvents(){
+
+
+    document.body.onkeydown = (e) => {
+      const key = e.key;
+      if(key === 'Enter'){
+          submitGuess();
+          }
+      
+
+      if(key === 'Backspace'){
+          removeLetter();
+      }
+
+      if(isLetter(key)){
+        addLetter(key);
+      }
+  
+    };
+
+}
+  
+function isLetter(key){
+    return key.length === 1 && key.match(/[a-z]/i);
+}
+
+function addLetter(element) {
+
+
+    key = element;
     
-    <script src="morse.js"></script>
-</body>
-</html>
+    tempGuess += key
+
+    txtBox.innerHTML = tempGuess
+    return;
+
+}
+
+function removeLetter(){
+
+    tempGuess = tempGuess.substring(0, tempGuess.length-1);
+
+    txtBox.innerHTML = tempGuess
+    return;
+
+}
+
+  back.addEventListener('click', () => {
+
+    
+        removeLetter();
+
+});
+
+  enter.addEventListener('click', () => {
+    
+    submitGuess();
+});
+
+function submitGuess(){
+
+
+    let input = txtBox.innerHTML;
+
+    tempGuess = "";
+
+    txtBox.innerHTML = "";
+
+    console.log("txtBox: " + input);
+    console.log("Answear: " + currentWord);
+
+
+    if(input == currentWord){
+
+        formattedWord = currentWord[0].toUpperCase() + currentWord.substring(1,currentWord.length);
+        wordDisplay.innerHTML = formattedWord;
+        wordDisplay.style.fontSize = "30px";
+        txtBox.classList.add('right');
+
+        setTimeout(() => {
+
+            txtBox.classList.remove('right');
+            
+          }, "300");
+
+        setTimeout(() => {
+
+            displayWord();
+            
+          }, "700");
+    } else {
+
+            txtBox.classList.add('wrong');
+       
+        setTimeout(() => {
+
+            txtBox.classList.remove('wrong');
+            
+          }, "300");
+    }
+}
+
+var isDragging = false;
+var startX, scrollLeft;
+document.getElementById('wordDisplay').addEventListener('mousedown', startDrag);
+document.getElementById('wordDisplay').addEventListener('touchstart', startDrag);
+document.addEventListener('mousemove', drag);
+document.addEventListener('touchmove', drag);
+document.addEventListener('mouseup', stopDrag);
+document.addEventListener('touchend', stopDrag);
+
+function startDrag(e) {
+    isDragging = true;
+    startX = e.type === 'touchstart' ? e.touches[0].pageX : e.pageX;
+    scrollLeft = document.getElementById('key').scrollLeft;
+
+    // Prevent default behavior for touch events
+    if (e.type === 'touchstart') {
+        e.preventDefault();
+    }
+}
+
+function drag(e) {
+    if (!isDragging) return;
+
+    // Prevent default behavior for touch events
+    if (e.type === 'touchmove') {
+        e.preventDefault();
+    }
+
+    var x = e.type === 'touchmove' ? e.touches[0].pageX : e.pageX;
+    var walk = (x - startX) * 3; // Adjust the multiplier based on the desired drag speed
+    document.getElementById('key').scrollLeft = Math.max(scrollLeft - walk, 0);
+    console.log('Dragging:', walk);
+}
+
+function stopDrag() {
+    isDragging = false;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    displayWord();
+    getKeys();
+    registerKeyboardEvents();
+});
 
