ad760ea395e3bc5345bdbae5c7fe54215c0391f6
wesleyhat <80120977+wesleyhat@users.noreply.github.com>
2024-09-21 18:19:30 -0400

    Add files via upload

---
+++ ncaafScript.js
@@ -0,0 +1,227 @@
+function toHttps(url) {
+    return url.replace(/^http:/, 'https:');
+}
+
+function getPeriodString(period) {
+    switch (period) {
+        case 0: return "0";
+        case 1: return "1st";
+        case 2: return "2nd";
+        case 3: return "3rd";
+        case 4: return "4th";
+        default: return "Invalid period";
+    }
+}
+
+async function loadLocalJSON() {
+    const response = await fetch('ncaafInfo.json'); // Path to your local file
+    const data = await response.json();
+    
+    teamInfo = data;
+}
+
+loadLocalJSON();
+
+function formatGameDate(gameDate) {
+    const date = new Date(gameDate);
+    const day = date.toLocaleString('en-US', { weekday: 'short' });
+    const month = date.getMonth() + 1; // Months are zero-indexed
+    const dayOfMonth = date.getDate();
+    let hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Ensure two digits for minutes
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    return `${day} ${month}/${dayOfMonth} ${hours}:${minutes}${ampm}`;
+}
+
+let refreshInterval = 10000; // Default to 30 seconds
+let intervalId;
+
+async function getGamesForAllTeams() {
+    let tally = 0; // Initialize tally variable
+    const page = toHttps(`http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?limit=1000`);
+    const req = await fetch(page);
+    tally++; // Increment tally for the teams API call
+    const data = await req.json();
+    const events = data.events;
+    let gamesInfo = []; // To store the actual game data
+    let liveGameFound = false; // To track if there's a live game
+
+   
+    for (const event of events) {
+        const matchup = event.shortName;
+        const teams = matchup.split(" @ ");
+        const away = teams[0];
+        const home = teams[1];
+
+        let homeLogoUrl = "";
+        let homeColor = "";
+        let awayLogoUrl = "";
+        let awayColor = "";
+
+        let hasHomeLogo = true;
+        let hasAwayLogo = true;
+
+        homeLogoUrl = teamInfo[home]?.logo ?? "";
+        homeColor = teamInfo[home]?.color ?? "#121212";
+        awayLogoUrl = teamInfo[away]?.logo ?? "";
+        awayColor = teamInfo[away]?.color ?? "#121212";
+
+        // Determine if logos exist (check if logo URL is empty or "none")
+        hasHomeLogo = homeLogoUrl && homeLogoUrl !== "none";
+        hasAwayLogo = awayLogoUrl && awayLogoUrl !== "none";
+
+
+        let awayScore = event.competitions[0].competitors[0].score;
+        let homeScore = event.competitions[0].competitors[1].score;
+        let gameDate = new Date(event.date);
+        let homeText = "#ffffff";
+        let awayText = "#ffffff";
+        let period = event.status.period;
+        let clock = event.status.displayClock;
+        let quarter = getPeriodString(period);
+        let gameState = event.status.type.state;
+
+        let gameStatus = "";
+
+        if (gameState === "pre") {
+            gameStatus = "pre";
+        } else if (gameState === "post") {
+            gameStatus = "post";
+            if (homeScore > awayScore) {
+                awayColor = "#0d0b15";
+                awayText = "#8c899c";
+            } else if(homeScore < awayScore){
+                homeColor = "#0d0b15";
+                homeText = "#8c899c";
+            } 
+        } else {
+            gameStatus = "live";
+            liveGameFound = true; // A live game is found
+        }
+
+        if (gameStatus === "pre") {
+            homeScore = "--";
+            awayScore = "--";
+        }
+
+        gamesInfo.push({
+            date: gameDate,
+            homeTeam: home,
+            awayTeam: away,
+            homeScore: homeScore,
+            awayScore: awayScore,
+            homeLogo: homeLogoUrl,
+            awayLogo: awayLogoUrl,
+            homeColor: homeColor,
+            awayColor: awayColor,
+            homeText: homeText,
+            awayText: awayText,
+            quarter: quarter,
+            clock: clock,
+            gameStatus: gameStatus,
+            hasHomeLogo: hasHomeLogo,
+            hasAwayLogo: hasAwayLogo
+        });
+    }
+
+    // Now inject the gamesInfo into the page
+    displayGames(gamesInfo);
+
+    console.log("Total number of APIs called: " + tally)
+
+    // Hide the loading screen and show the games container
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('games-container').style.display = 'grid';
+
+    // Adjust refresh interval based on live game status
+    if (liveGameFound) {
+        if (!intervalId) {
+            refreshInterval = 10000; // Set to 30 seconds for live games
+            intervalId = setInterval(getGamesForAllTeams, refreshInterval); // Start refreshing
+        }
+    } else {
+        // If no live games, clear the interval
+        if (intervalId) {
+            clearInterval(intervalId);
+            intervalId = null; // Reset intervalId
+        }
+    }
+}
+
+// Function to display games on the webpage
+function displayGames(games) {
+    const container = document.getElementById('games-container');
+    container.innerHTML = ''; // Clear any previous content
+    const containerPast = document.getElementById('games-container-past');
+    containerPast.innerHTML = ''; // Clear any previous content
+
+    games.forEach(game => {
+        const gameDiv = document.createElement('div');
+        gameDiv.classList.add('game');
+
+        const awayTeamDiv = document.createElement('div');
+        awayTeamDiv.classList.add('team');
+        awayTeamDiv.innerHTML = `
+                <img src="${game.awayLogo}" alt="${game.awayTeam} Logo">
+                <span class="score">${game.awayScore}</span>
+            `;
+
+        if(!game.hasAwayLogo){
+            awayTeamDiv.innerHTML = `
+                <h2 style="font-size: 20px; font-weight: 400;">${game.awayTeam}</h2>
+                <span class="score">${game.awayScore}</span>
+            `;
+        }
+
+        const homeTeamDiv = document.createElement('div');
+        homeTeamDiv.classList.add('team');
+        homeTeamDiv.innerHTML = `
+            <img src="${game.homeLogo}" alt="${game.homeTeam} Logo">
+            <span class="score">${game.homeScore}</span>
+        `;
+
+        if(!game.hasHomeLogo){
+            homeTeamDiv.innerHTML = `
+            <h2 style="font-size: 20px; font-weight: 400;">${game.homeTeam}</h2>
+            <span class="score">${game.homeScore}</span>
+        `;
+        }
+
+
+        homeTeamDiv.style.backgroundColor = game.homeColor;
+        awayTeamDiv.style.backgroundColor = game.awayColor;
+        homeTeamDiv.style.color = game.homeText;
+        awayTeamDiv.style.color = game.awayText;
+
+        const gameDateDiv = document.createElement('div');
+        gameDateDiv.classList.add('game-date');
+        gameDateDiv.textContent = `${formatGameDate(game.date)}`;
+
+        if (game.gameStatus === "post") {
+            if(game.homeScore < game.awayScore){
+                gameDiv.appendChild(awayTeamDiv);
+                gameDiv.appendChild(homeTeamDiv);
+                gameDiv.appendChild(gameDateDiv);
+            } else {
+                gameDiv.appendChild(homeTeamDiv);
+                gameDiv.appendChild(awayTeamDiv);
+                gameDiv.appendChild(gameDateDiv);
+            }
+            containerPast.appendChild(gameDiv);
+            gameDateDiv.textContent = "Final";
+        } else {
+
+            gameDiv.appendChild(awayTeamDiv);
+            gameDiv.appendChild(homeTeamDiv);
+            gameDiv.appendChild(gameDateDiv);
+            if (game.gameStatus === "live") {
+                gameDateDiv.innerHTML = `<span style="color: #e13534;">Live</span>&nbsp;&nbsp;&nbsp;${game.quarter}&nbsp;&nbsp;${game.clock}`;
+            }
+            container.appendChild(gameDiv);
+        }
+    });
+}
+
+getGamesForAllTeams();
