97606445cebdd9a3922a4195fa2e47d4de23cb54
wesleyhat <80120977+wesleyhat@users.noreply.github.com>
2025-05-08 12:06:56 -0400

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -35,7 +35,7 @@ document.body.onkeyup = function(e) {
             context.fillStyle = 'rgba(0, 0, 0, 0.05)';
             context.fillRect(0, 0, canvas.width, canvas.height);
         
-            context.fillStyle = '#0F0';
+            context.fillStyle = '#b4befe';
             context.font = fontSize + 'px monospace';
         
             for(let i = 0; i < rainDrops.length; i++)
@@ -59,7 +59,7 @@ addEventListener("touchstart", function(event){
             context.fillStyle = 'rgba(0, 0, 0, 0.05)';
             context.fillRect(0, 0, canvas.width, canvas.height);
         
-            context.fillStyle = '#0F0';
+            context.fillStyle = '#b4befe';
             context.font = fontSize + 'px monospace';
         
             for(let i = 0; i < rainDrops.length; i++)
